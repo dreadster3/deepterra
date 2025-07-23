eee8b4cf3bfc299dc292cb90770dbf3d39e28e9a
dreadster3 <afonso.antunes@live.com.pt>
2025-07-23 01:47:12 +0100

    wip: dumb retrieve resource types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use hcl::Body;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./terraform/main.tf").expect("Failed to read file");
+    let body: Body = hcl::from_str(contents.as_str()).unwrap();
+
+    for block in body.blocks() {
+        if block.identifier() == "resource" {
+            let labels = block.labels();
+            let resource_type = labels[0].as_str();
+            println!("{resource_type:#?}");
+        }
+    }
 }
